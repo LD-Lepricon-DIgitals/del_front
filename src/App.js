e71c6c7386a3d2bf11e4dff7a226515e4189e2b4
rybmks <rybalkomaks3@gmail.com>
2024-11-28 16:12:40 +0000

    first version of order modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Main from './pages/main/Main.jsx';
-import Cart from './pages/cart/Cart.jsx'
-import Profile from './pages/profile/Profile.jsx';
-import Menu from './pages/menu/Menu.jsx';
+
+
+// Лениво загружаемые компоненты
+const Main = lazy(() => import('./pages/main/Main.jsx'));
+const Profile = lazy(() => import('./pages/profile/Profile.jsx'));
+const Menu = lazy(() => import('./pages/menu/Menu.jsx'));
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/menu" element={<Menu />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/menu" element={<Menu />} />
+          </Routes>
+        </Suspense>
+      </Router>
   );
 }
 
