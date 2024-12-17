f4a403ac6dbf4f35bedb1d9199ec02b4b91ee664
FUZI <k.lipovok@gmail.com>
2024-12-17 22:04:19 +0200

    ListItem component added

--- src/pages/menu/menu_item.jsx
+++ src/pages/menu/menu_item.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from './menu_item.module.css'
+import ListItem from "../shared/ListItem/ListItem";
 
-function MenuItem(props){
-    
-    return(
-        <div className={styles["item"]}>
+function MenuItem(props) {
+    return (
+        <ListItem>
             <img src={props.img} className={styles["dish-ico"]}/>
-                <p>{props.name}</p>
+            <p>{props.name}</p>
             <div className={styles["container"]}>
                 <p>{props.category}</p>
                 <p>{props.price}</p>
             </div>
-        </div>
+        </ListItem>
     );
-
 }
 
 export default MenuItem;
